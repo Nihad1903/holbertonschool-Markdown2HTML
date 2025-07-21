5560e8cd09767348c0647c9c72a189b8531c362c
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 14:34:23 +0400

    DEMO

--- markdown2html.py
+++ markdown2html.py
@@ -21,6 +21,15 @@ def markdownparser():
 
     sys.exit(0)
 
+def headings():
+    with open("README.html", "w") as output:
+        with open(sys.argv[1], 'r') as file:
+            for l in file.readlines():
+                num = l.count('#')
+                text = l.replace("#", "")[1:-1]
+                new_line = f"<h{num}>{text}</h{num}>"
+                output.write(new_line + "\n")
 
 if __name__ == "__main__":
-    markdownparser()
+    # markdownparser()
+    headings()
