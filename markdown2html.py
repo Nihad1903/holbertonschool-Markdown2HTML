ce630ca840742e9b482b3e595151c3623c37daef
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 14:35:47 +0400

    DEMO

--- markdown2html.py
+++ markdown2html.py
@@ -25,9 +25,9 @@ def markdownparser():
 def headings():
     with open("README.html", "w") as output:
         with open(sys.argv[1], 'r') as file:
-            for l in file.readlines():
-                num = l.count('#')
-                text = l.replace("#", "")[1:-1]
+            for line in file.readlines():
+                num = line.count('#')
+                text = line.replace("#", "")[1:-1]
                 new_line = f"<h{num}>{text}</h{num}>"
                 output.write(new_line + "\n")
 
