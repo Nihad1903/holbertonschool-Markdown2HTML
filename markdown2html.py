320e1a45d67a57ab903777a7c125d54bad044993
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 11:27:30 +0400

    demo

---
+++ markdown2html.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def markdownparser(md_name, md_file):
+    print(len(sys.argv))
+
+
+
+
+if __name__ == "__main__":
+    md_name = sys.argv[0]
+    
+    print("hello")
