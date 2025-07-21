fc9bdeeb87d44f4e78412777d16ef2c93420334d
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 14:24:36 +0400

    DEMO

--- markdown2html.py
+++ markdown2html.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Converts a Markdown file to an HTML file."""
 import sys
 import os
+def markdownparser():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
 
-def main():
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    # Check if the Markdown file exists
     if not os.path.exists(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
-    # Check if the number of arguments is correct
-    if len(sys.argv) == 1 or len(sys.argv) == 2:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        sys.exit(1)
-    # Convert Markdown to HTML
+
     sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    markdownparser()
