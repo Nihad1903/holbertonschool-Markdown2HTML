0078a6deb2ed7d04132c01aa0a79f8814e9a50fc
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 14:31:50 +0400

    DEMO

--- markdown2html.py
+++ markdown2html.py
@@ -8,11 +8,9 @@ import os
 
 def markdownparser():
     if len(sys.argv) < 3:
-        print(
-            "Usage: ./markdown2html.py README.md "
-            "README.html",
-            file=sys.stderr
-        )
+        usage = "Usage: ./markdown2html.py README.md README.html"
+        print(usage, file=sys.stderr)
+        sys.exit(1)
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
