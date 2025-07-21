eb649bf002a64d4bf1ab0c94f368ee97b431b45d
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 11:54:23 +0400

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -2,12 +2,13 @@
 import sys
 
 def markdownparser(md_name, md_file):
-    print(len(sys.argv))
+    num_of_args = len(sys.argv) - 1
 
-
-
+    if num_of_args < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        return 1
+    elif ".md" not in sys.argv[1]:
+        print("Missing <README.md>", file=sys.stderr)
 
 if __name__ == "__main__":
-    md_name = sys.argv[0]
-    
-    print("hello")
+    markdownparser(md_name='test', md_file='test')
