ce8a801d831b9599966d789e1328c6174d8034b4
Nihad <nihadtaghiyev1@gmail.com>
2025-07-21 13:58:31 +0400

    DEMO

--- markdown2html.py
+++ markdown2html.py
@@ -4,19 +4,19 @@ import sys
 import os
 
 def main():
-    if len(sys.argv[1:]) != 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
-
-    # Store the arguments into variables
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    # Check that the markdown file exist and it is a file
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f'Missing {input_file}', file=sys.stderr)
+    # Check if the Markdown file exists
+    if not os.path.exists(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
+    # Check if the number of arguments is correct
+    if len(sys.argv) == 1 or len(sys.argv) == 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+    # Convert Markdown to HTML
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
